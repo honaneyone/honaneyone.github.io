250f992e8e356cf2a73e662a3af015d294e6c2b5
honaneyoka <honaneyoka.zzz@gmail.com>
2021-08-29 04:29:36 +0900

    fix code style

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import Layout, {siteTitle} from '../../components/layout'
+import Layout, { siteTitle } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
-import {getSortedPostsData} from '../../lib/posts'
+import { getSortedPostsData } from '../../lib/posts'
 import Link from 'next/link'
 import Date from '../../components/date'
-import {GetStaticProps} from 'next'
+import { GetStaticProps } from 'next'
 
-export default function BlogIndex({allPostsData}: {
+export default function BlogIndex({ allPostsData }: {
     allPostsData: {
         date: string
         title: string
@@ -21,7 +21,7 @@ export default function BlogIndex({allPostsData}: {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({id, date, title}) => (
+                    {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href={`/blog/${id}`}>
                                 <a>{title}</a>
